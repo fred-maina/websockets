0b0eb22e18dc660000f7614e2783496bc437d29b
Fredrick Maina <fchege04@gmail.com>
2024-11-11 17:54:23 +0300

    inital commit

---
+++ websockets/src/test/java/com/fred/websockets/WebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fred.websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
